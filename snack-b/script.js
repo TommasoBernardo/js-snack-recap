f0f7ad44d8f0c4cdcfd5893ecb495dd1016431fb
TommasoBernardo <tommasob1210@gmail.com>
2022-12-22 10:23:58 +0100

    create snack-b

---
+++ snack-b/script.js
@@ -0,0 +1,5 @@
+/***
+Chiedere all'API 10 nomi da inserire in un array di invitati.
+Una volta generata la lista chiedere all'utente di dire il proprio nome.
+Se è uno degli invitati ritornare un messaggio di benvenuto, altrimenti di accesso negato.
+*/
